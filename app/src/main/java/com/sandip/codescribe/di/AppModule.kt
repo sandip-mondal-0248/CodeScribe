ed0639a6c1593fd212c1d69b035c0eb57ff52a6f
Sandip Mondal <sandipmondal0248@gmail.com>
2024-04-13 01:10:00 +0530

    Add EditNoteViewModel

--- app/src/main/java/com/sandip/codescribe/di/AppModule.kt
+++ app/src/main/java/com/sandip/codescribe/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.sandip.codescribe.feature_note.data.repository.NoteRepositoryImpl
 import com.sandip.codescribe.feature_note.domain.repository.NoteRepository
 import com.sandip.codescribe.feature_note.domain.use_case.AddNote
 import com.sandip.codescribe.feature_note.domain.use_case.DeleteNote
+import com.sandip.codescribe.feature_note.domain.use_case.GetNote
 import com.sandip.codescribe.feature_note.domain.use_case.GetNotes
 import com.sandip.codescribe.feature_note.domain.use_case.NoteUseCases
 import dagger.Module
@@ -41,7 +42,8 @@ object AppModule {
         return NoteUseCases(
             getNotes = GetNotes(repository),
             deleteNote = DeleteNote(repository),
-            addNote = AddNote(repository)
+            addNote = AddNote(repository),
+            getNote = GetNote(repository)
         )
     }
 }
--- app/src/main/java/com/sandip/codescribe/feature_note/prsentation/add_edit_note/NoteTextFieldState.kt
+++ app/src/main/java/com/sandip/codescribe/feature_note/prsentation/add_edit_note/NoteTextFieldState.kt
@@ -1,3 +1,7 @@
 package com.sandip.codescribe.feature_note.prsentation.add_edit_note
 
-data class NoteTextFieldState()
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isHintVisible: Boolean = true
+)
